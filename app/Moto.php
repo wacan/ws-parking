869fc9118cbd969ce8e3dc2d15b88371fcc4129d
wacan <65459249+wacan@users.noreply.github.com>
2020-09-15 22:09:38 -0500

    4 entrega
    se Realizan realciones de los modelos para asignar el parqueadero,
    
    Queda pendiente el modulo de reportes y el panel

--- app/Moto.php
+++ app/Moto.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Moto extends Model
 {
-    //
+        //funciones para definir y validar el parking Motos
+        public function parkings()
+        {
+            return $this->belongsToMany(Parking::class)->withTimestamps();
+        }
+    
+        public function asignarParking($parking)
+        {
+            $this->parkings()->sync($parking, false);
+        }
+    
+        public function tieneParking()
+        {
+            return $this->parkings->flatten()->pluck('name')->unique();
+        }
 }
